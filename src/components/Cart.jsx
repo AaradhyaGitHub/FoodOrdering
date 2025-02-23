36f96cd74f8e7d1ef7d508ab4251e8d39fa32533
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-02-23 12:04:05 -0800

    Fixed modal glitch on esc key

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -20,12 +20,16 @@ export default function Cart() {
     userProgressCtx.hideCart();
   }
 
-  function handleGoToCheckout(){
+  function handleGoToCheckout() {
     userProgressCtx.showCheckout();
   }
 
   return (
-    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+    <Modal
+      className="cart"
+      open={userProgressCtx.progress === "cart"}
+      onClose={userProgressCtx.progress === "cart" ? handleCloseCart : null}
+    >
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
