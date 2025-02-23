83acd307b66303df142dc9b057d8977c71062715
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-02-22 17:15:23 -0800

    Added Dynamic Modal Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,10 @@ import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
 export default function Header() {
-  const cartCtx = useContext(CartContext)
-  const totalCartItems = cartCtx.items.reduce(
-    (totalNumberOfItems, item) => {
-      return totalNumberOfItems + item.quantity;
-    }, 0
-  );
+  const cartCtx = useContext(CartContext);
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
