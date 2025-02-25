4780327818a85fae454f567ca2885245f76647db
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-02-24 19:47:19 -0800

    Implemented custom hook

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,9 +5,12 @@ import { currencyFormatter } from "../util/formatting";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import 
+
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
+  
   const cartTotal = cartCtx.items.reduce(
     (finalPrice, item) => finalPrice + item.quantity * item.price,
     0
@@ -16,33 +19,38 @@ export default function Checkout() {
   function handleClose() {
     userProgressCtx.hideCheckout();
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData
+        }
+      })
+    });
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" type="text" name="full-name" id="full-name" />
-        <Input
-          label="E-mail Address"
-          type="email"
-          name="full-name"
-          id="email"
-        />
-        <Input
-          label="Street Address"
-          type="text"
-          name="full-name"
-          id="street"
-        />
+        <Input label="Full Name" type="text" name="name" id="name" />
+        <Input label="E-mail Address" type="email" name="email" id="email" />
+        <Input label="Street Address" type="text" name="street" id="street" />
         <div className="control-row">
-          <Input
-            label="Postal Coded"
-            type="text"
-            name="full-name"
-            id="postal-code"
-          />
-          <Input label="City" type="text" name="full-name" id="city" />
+          <Input label="Postal Code" type="text" name="postal-code" id="postal-code" />
+          <Input label="City" type="text" name="city" id="city" />
         </div>
         <p className="modal-actions">
           <Button textOnly type="button" onClick={handleClose}>
