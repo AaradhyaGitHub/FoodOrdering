a0846c73c00a3b194aba26f543e415bc2d977b4b
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-04 17:30:26 -0800

    Index.css Update, Migration to form action,project finsihed

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,33 +1,32 @@
-import { useContext } from "react";
-import Modal from "./UI/Modal";
-import CartContext from "../store/CartContext";
-import { currencyFormatter } from "../util/formatting";
-import Input from "./UI/Input";
-import Button from "./UI/Button";
-import UserProgressContext from "../store/UserProgressContext";
-import useHttp from "../custom hooks/useHttp.js";
+import { useContext, useActionState } from 'react';
+
+import Modal from './UI/Modal.jsx';
+import CartContext from '../store/CartContext.jsx';
+import { currencyFormatter } from '../util/formatting.js';
+import Input from './UI/Input.jsx';
+import Button from './UI/Button.jsx';
+import UserProgressContext from '../store/UserProgressContext.jsx';
+import useHttp from '../custom hooks/useHttp.js';
+import Error from './Error.jsx';
 
 const requestConfig = {
-  method: "POST",
+  method: 'POST',
   headers: {
-    "Content-Type": "application/json"
-  }
+    'Content-Type': 'application/json',
+  },
 };
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
 
-  const {
-    data,
-    isLoading: isSending,
-    error,
-    sendRequest,
-    clearData
-  } = useHttp("http://localhost:3000/orders", requestConfig);
+  const { data, error, sendRequest, clearData } = useHttp(
+    'http://localhost:3000/ordersss',
+    requestConfig
+  );
 
   const cartTotal = cartCtx.items.reduce(
-    (finalPrice, item) => finalPrice + item.quantity * item.price,
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
@@ -41,35 +40,24 @@ export default function Checkout() {
     clearData();
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const fd = new FormData(event.target);
-    const customerData = Object.fromEntries(fd.entries());
+  async function checkoutAction(prevState, fd) {
+    const customerData = Object.fromEntries(fd.entries()); // { email: test@example.com }
 
-    sendRequest({
-      order: {
-        items: cartCtx.items,
-        customer: customerData
-      }
-    });
-
-    fetch("http://localhost:3000/orders", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
+    await sendRequest(
+      JSON.stringify({
         order: {
           items: cartCtx.items,
-          customer: customerData
-        }
+          customer: customerData,
+        },
       })
-    });
+    );
   }
+
+  const [formState, formAction, isSending] = useActionState(checkoutAction,null);
 
   let actions = (
     <>
-      <Button textOnly type="button" onClick={handleFinish}>
+      <Button type='button' textOnly onClick={handleClose}>
         Close
       </Button>
       <Button>Submit Order</Button>
@@ -83,13 +71,16 @@ export default function Checkout() {
   if (data && !error) {
     return (
       <Modal
-        open={userProgressCtx.progress === "checkout"}
-        onClose={handleClose}
+        open={userProgressCtx.progress === 'checkout'}
+        onClose={handleFinish}
       >
         <h2>Success!</h2>
-        <p>Your order was submitted successfully</p>
-        <p>We will reach out to you via email regarding your order status </p>
-        <p className="modal-actions">
+        <p>Your order was submitted successfully.</p>
+        <p>
+          We will get back to you with more details via email within the next
+          few minutes.
+        </p>
+        <p className='modal-actions'>
           <Button onClick={handleFinish}>Okay</Button>
         </p>
       </Modal>
@@ -97,25 +88,22 @@ export default function Checkout() {
   }
 
   return (
-    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form onSubmit={handleSubmit}>
+    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
+      <form action={formAction}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" type="text" name="name" id="name" />
-        <Input label="E-mail Address" type="email" name="email" id="email" />
-        <Input label="Street Address" type="text" name="street" id="street" />
-        <div className="control-row">
-          <Input
-            label="Postal Code"
-            type="text"
-            name="postal-code"
-            id="postal-code"
-          />
-          <Input label="City" type="text" name="city" id="city" />
+        <Input label='Full Name' type='text' id='name' />
+        <Input label='E-Mail Address' type='email' id='email' />
+        <Input label='Street' type='text' id='street' />
+        <div className='control-row'>
+          <Input label='Postal Code' type='text' id='postal-code' />
+          <Input label='City' type='text' id='city' />
         </div>
-        {error && <Error title="Failed to submit order" message={error} />}
-        <p className="modal-actions">{actions}</p>
+
+        {error && <Error title='Failed to submit order' message={error} />}
+
+        <p className='modal-actions'>{actions}</p>
       </form>
     </Modal>
   );
--- src/custom hooks/useHttp.js
+++ src/custom hooks/useHttp.js
@@ -1,21 +1,23 @@
-import { useState, useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from 'react';
 
 async function sendHttpRequest(url, config) {
   const response = await fetch(url, config);
+
   const resData = await response.json();
 
   if (!response.ok) {
     throw new Error(
-      resData.message || "Something went wrong, failed to send request"
+      resData.message || 'Something went wrong, failed to send request.'
     );
   }
+
   return resData;
 }
 
 export default function useHttp(url, config, initialData) {
   const [data, setData] = useState(initialData);
-  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   function clearData() {
     setData(initialData);
@@ -23,22 +25,12 @@ export default function useHttp(url, config, initialData) {
 
   const sendRequest = useCallback(
     async function sendRequest(data) {
-      console.log("Sending request to:", url);
-      console.log(
-        "Request config:",
-        JSON.stringify({ ...config, body: JSON.stringify(data) })
-      );
-
       setIsLoading(true);
       try {
-        const resData = await sendHttpRequest(url, {
-          ...config,
-          headers: { "Content-Type": "application/json", ...config.headers },
-          body: JSON.stringify(data)
-        });
+        const resData = await sendHttpRequest(url, { ...config, body: data });
         setData(resData);
       } catch (error) {
-        setError(error.message || "Something went wrong");
+        setError(error.message || 'Something went wrong!');
       }
       setIsLoading(false);
     },
@@ -46,10 +38,16 @@ export default function useHttp(url, config, initialData) {
   );
 
   useEffect(() => {
-    if (!config.method || config.method === "GET") {
+    if ((config && (config.method === 'GET' || !config.method)) || !config) {
       sendRequest();
     }
   }, [sendRequest, config]);
 
-  return { data, isLoading, error, sendRequest, clearData };
+  return {
+    data,
+    isLoading,
+    error,
+    sendRequest,
+    clearData
+  };
 }
